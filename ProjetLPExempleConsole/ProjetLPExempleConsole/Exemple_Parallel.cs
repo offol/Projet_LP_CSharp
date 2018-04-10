f996e4e74e1e81b56ece1c931e9ab781a4f4d3b7
François-Xavier Mousseau <fxmousseau@hotmail.com>
2018-04-10 15:43:37 -0400

    changement de l'interface

---
+++ ProjetLPExempleConsole/ProjetLPExempleConsole/Exemple_Parallel.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjetLPExempleConsole
+{
+    public static class Exemple_Parallel
+    {
+        private static List<Tuple<double, int>> resultList = new List<Tuple<double, int>>();
+
+        private static List<double> inputList = new List<double>() {
+            2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26
+        };
+
+        private static string[] testArray =
+         {
+            "1. Vert",
+            "2. Rouge",
+            "3. Jaune",
+            "4. Bleu",
+            "5. Orange",
+            "6. Mauve",
+            "7. Violet",
+            "8. Brun",
+            "9. Rose",
+            "10. Blanc",
+            "11. Noir"
+        };
+
+        /// <summary>
+        /// Exemple de parallel for loop
+        /// </summary>
+        public static void Exemple_For()
+        {
+            Console.WriteLine("Start For exemple");
+
+            for (int y = 0; y <= 10; y++)
+            {
+                Console.WriteLine("{0}, Thread Id= {1}", testArray[y], Thread.CurrentThread.ManagedThreadId);
+            };
+
+            Console.WriteLine("Fin de la boucle For \n");
+
+            //Exemple de Parallel for loop
+            Parallel.For(0, 11, index =>
+            {
+                Console.WriteLine("{0}, Thread Id= {1}", testArray[index], Thread.CurrentThread.ManagedThreadId);
+            });
+
+            Console.WriteLine("End For exemple");
+        }
+        //***********************************************************************************
+
+        /// <summary>
+        /// Exemple foreach loop
+        /// </summary>
+        public static void Exemple_Foreach()
+        {
+            Console.WriteLine("Start Foreach exemple");
+
+            foreach (var item in testArray)
+            {
+                Console.WriteLine("{0}, Thread Id= {1}", item, Thread.CurrentThread.ManagedThreadId);
+            };
+
+            Console.WriteLine("Fin de la boucle foreach \n");
+
+            //Exemple de  Parallel foreach
+            Parallel.ForEach<string>(testArray, item =>
+            {
+                //Faire quelque chose au élément de la liste
+                Console.WriteLine("{0}, Thread Id= {1}", item, Thread.CurrentThread.ManagedThreadId);
+            });
+
+            Console.WriteLine("End Foreach exemple");
+        }
+        //***********************************************************************************
+
+        /// <summary>
+        /// Exemple PLINQ
+        /// </summary>
+        public static void Exemple_plinq()
+        {
+            Console.WriteLine("Start PLINQ exemple");
+
+            //Exemple de PLINQ
+            resultList = inputList.AsParallel().Select(val => new Tuple<double, int>((val / 8), Thread.CurrentThread.ManagedThreadId)).ToList();
+
+            foreach (var item in resultList)
+            {
+                Console.WriteLine("Résulat: " + item.Item1 + "\t Thread: " + item.Item2);
+            }
+
+            Console.WriteLine("End PLINQ exemple");
+        }
+        //********************************************************************************************************************************************
+
+            /// <summary>
+            /// Exemple Parallel Invoke
+            /// </summary>
+        public static void Exemple_invoke()
+        {
+
+            Parallel.Invoke(() =>
+            {
+                Console.WriteLine("Begin first task...");
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("End First Task");
+
+            },
+             () =>
+             {
+                 Console.WriteLine("Begin second task...");
+                 for (int i = 6; i <= 10; i++)
+                 {
+                     Console.WriteLine(i);
+                 }
+                 Console.WriteLine("End second Task");
+
+             },
+             () =>
+             {
+                 Console.WriteLine("Begin third task...");
+                 for (int i = 11; i <= 15; i++)
+                 {
+                     Console.WriteLine(i);
+                 }
+                 Console.WriteLine("End third Task");
+
+             }
+             );
+            Console.WriteLine("Returned from Parallel.Invoke");
+        }
+    }
+}
--- ProjetLPExempleConsole/ProjetLPExempleConsole/Program.cs
+++ ProjetLPExempleConsole/ProjetLPExempleConsole/Program.cs
@@ -9,31 +9,19 @@ namespace ProjetLPExempleConsole
 {
     class Program
     {
-        private static List<Tuple<double,int>> resultList = new List<Tuple<double, int>>();
-
-        private static List<double> inputList = new List<double>() {
-            2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26
-        };
-
-        static string[] testArray =
-        {
-            "1. Vert",
-            "2. Rouge",
-            "3. Jaune",
-            "4. Bleu",
-            "5. Orange",
-            "6. Mauve",
-            "7. Violet",
-            "8. Brun",
-            "9. Rose",
-            "10. Blanc",
-            "11. Noir"
-        };
+        private static bool _quitter = false;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Écrire un numéro de 1 a 5");
-            for (int i = 1; i <= 5; i++)
+            Console.WriteLine("Écrire un chiffre de 1 a 5");
+            Console.WriteLine("1. Exemple task");
+            Console.WriteLine("2. Exemple Invoke");
+            Console.WriteLine("3. Exemple ForEach");
+            Console.WriteLine("4. Exemple For");
+            Console.WriteLine("5. Exemple PLinq");
+            Console.WriteLine("Q. Exit");
+
+            while (!_quitter)
             {
                 ConsoleKeyInfo cki;
                 cki = Console.ReadKey(true);
@@ -53,66 +41,35 @@ namespace ProjetLPExempleConsole
                     else if (Int32.Parse(cki.KeyChar.ToString()) == 2)
                     {
                         //Exemple de Parallel invoke
-                        ParallelInvoke();
+                        Exemple_Parallel.Exemple_invoke();
                     }
                     else if (Int32.Parse(cki.KeyChar.ToString()) == 3)
                     {
-                        Console.WriteLine("Start Foreach exemple");
-
-                        foreach(var item in testArray)
-                        {
-                            Console.WriteLine("{0}, Thread Id= {1}", item, Thread.CurrentThread.ManagedThreadId);
-                        };
-
-                        Console.WriteLine("Fin de la boucle foreach");
-
-                        //Exemple de  Parallel foreach
-                        Parallel.ForEach<string>(testArray, item =>
-                        {
-                            //Faire quelque chose au élément de la liste
-                            Console.WriteLine("{0}, Thread Id= {1}", item, Thread.CurrentThread.ManagedThreadId);
-                        });
-
-                        Console.WriteLine("End Foreach exemple");
+                        //Exemple foreach
+                        Exemple_Parallel.Exemple_Foreach();
                     }
                     else if (Int32.Parse(cki.KeyChar.ToString()) == 4)
                     {
-                        Console.WriteLine("Start For exemple");
-
-                        for (int y = 0; y <= 10; y++)
-                        {
-                            Console.WriteLine("{0}, Thread Id= {1}", testArray[y], Thread.CurrentThread.ManagedThreadId);
-                        };
-
-                        Console.WriteLine("Fin de la boucle For");
-
-                        //Exemple de Parallel for loop
-                        Parallel.For(0, 11, index =>
-                        {
-                            Console.WriteLine("{0}, Thread Id= {1}", testArray[index], Thread.CurrentThread.ManagedThreadId);
-                        });
-
-                        Console.WriteLine("End For exemple");
+                        //Exemple for loop
+                        Exemple_Parallel.Exemple_For();
                     }
                     else if (Int32.Parse(cki.KeyChar.ToString()) == 5)
                     {
-                        Console.WriteLine("Start PLINQ exemple");
-
-                        //Exemple de PLINQ
-                        resultList = inputList.AsParallel().Select(val => new Tuple<double, int> ((val / 8), Thread.CurrentThread.ManagedThreadId)).ToList();
-
-                        foreach (var item in resultList)
-                        {
-                            Console.WriteLine("Résulat: " + item.Item1 + "\t Thread: "+ item.Item2);
-                        }
-
-                        Console.WriteLine("End PLINQ exemple");
+                        //exemple plinq
+                        Exemple_Parallel.Exemple_plinq();
                     }
+
+
                 }
+                else if (cki.KeyChar.ToString() == "q" || cki.KeyChar.ToString() == "Q")
+                {
+                    _quitter = true;
+                }
+
             }
 
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey();
+           /* Console.WriteLine("Press any key to exit");
+            Console.ReadKey();*/
         }
 
         private static async void Test()
@@ -139,41 +96,6 @@ namespace ProjetLPExempleConsole
             Console.WriteLine("End Test2");
         }
 
-        private static void ParallelInvoke()
-        {
-
-            Parallel.Invoke(() =>
-            {
-                Console.WriteLine("Begin first task...");
-                for (int i = 0; i <= 5; i++)
-                {
-                    Console.WriteLine(i);
-                }
-                Console.WriteLine("End First Task");
-
-            },
-             () =>
-             {
-                 Console.WriteLine("Begin second task...");
-                 for (int i = 6; i <= 10; i++)
-                 {
-                     Console.WriteLine(i);
-                 }
-                 Console.WriteLine("End second Task");
-
-             },
-             () =>
-             {
-                 Console.WriteLine("Begin third task...");
-                 for (int i = 11; i <= 15; i++)
-                 {
-                     Console.WriteLine(i);
-                 }
-                 Console.WriteLine("End third Task");
-
-             }
-             );
-            Console.WriteLine("Returned from Parallel.Invoke");
-        }
+       
     }
 }
